4811ee7f76e7f9a2c884195b35bb1d2350b90a83
Juan David Bohorquez Grimaldo <juan_david.bohorquez@uao.edu.co>
2023-11-12 11:26:23 -0500

    Add buttons for the other plataforms like Google and Github for example and finish register module

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import userRegisterModal from '../../hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../Inputs/Input';
+import toast from 'react-hot-toast';
+import Button from '../Button';
 const RegisterModal = () => {
     const registerModal = userRegisterModal();
     const [isLoading, setIsLoading] = useState(false);
@@ -34,7 +36,7 @@ const RegisterModal = () => {
                 registerModal.onClose();
             })
             .catch((err) => {
-                console.log(err);
+                toast.error('Something went wrong.');
             })
             .finally(() => {
                 setIsLoading(false);
@@ -49,8 +51,26 @@ const RegisterModal = () => {
              />
             <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
             <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
-            <Input id="password" label="Password" disabled={isLoading} register={register} errors={errors} required />
+            <Input id="password" type="password" label="Password" disabled={isLoading} register={register} errors={errors} required />
         </div>    
+    );
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
+            <Button outline label="Continue with GitHub" icon={AiFillGithub} onClick={() => {}} />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div onClick={registerModal.onClose} className="justify-center text-center flex flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div className="text-neutral-800 cursor-pointer hover:underline">
+                        Log in
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
     return ( 
@@ -62,6 +82,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
             />
     );         
 }
