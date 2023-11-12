38f06909ae634d29577453e427b93b32eec2fdd3
Juan David Bohorquez Grimaldo <juan_david.bohorquez@uao.edu.co>
2023-11-12 18:48:13 -0500

    Add categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 
 interface NavbarProps {
     currentUser?: safeUser | null;
@@ -23,6 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({
                 </div>
             </Container>
         </div>
+        <Categories />
      </div>
     );
 }
