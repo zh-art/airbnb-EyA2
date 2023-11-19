fd909ecf60aa42ac5647208ebb656c10d6b5ed90
Jaime Nanez <120343724+Jaime2003z@users.noreply.github.com>
2023-11-19 13:20:07 -0500

    Hooks agregados y correción de algunos errores

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,36 @@ import Image from 'next/image'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-export default function Home() {
-  const isEmpty = true;
-
-  if(isEmpty) {
-    return(
+export default async function Home() {
+  const Listings = await getListings();
+  const currentUser = await getCurrentUser();
+  if (Listings.length === 0) {
+    return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
     )
   }
-  
+
   return (
-      <ClientOnly>
-         <Container>
-          <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-            <div>My future listings</div>
-          </div>
-         </Container>
-      </ClientOnly>
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {Listings.map((listing: any) => {
+            return(
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
